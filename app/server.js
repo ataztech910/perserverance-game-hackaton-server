d10ff0df2b78d7e78a88e5f5c32cb1082261ec8d
Andrei Tazetdinov <webconsult.ekb@gmail.com>
2020-05-10 19:10:26 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,8 +7,15 @@ const path = require('path')
 
 const app = express()
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ // Add this
+ if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
+      res.header('Access-Control-Max-Age', 120);
+      return res.status(200).json({});
+  }
+  next();
 });
 
 const server = http.Server(app)
